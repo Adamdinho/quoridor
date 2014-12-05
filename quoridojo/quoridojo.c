276c16aa70317d11405da4e4bb858985b12025e1
adamdinho <adam.d.pickersgill@gmail.com>
2014-12-05 14:13:06 -0500

    Refactoring directories and packages

---
+++ quoridojo/quoridojo.c
@@ -0,0 +1,14 @@
+/*************************************************************
+* \file quoridojo.c
+* ECSE 456 Design Project
+* Creation Date : 10/29/2014
+* Author: Adam Pickersgill  <adam.pickersgill@mail.mcgill.ca>
+*
+* Description: Currently a stub class to test integrity of the
+* build process.
+**************************************************************/
+
+
+int main() {
+  
+}
